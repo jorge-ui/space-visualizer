bdeae38a15bc940b0b90efe9c9c503fecacd84cd
Jorge Rivera <jrdeveloper71@gmail.com>
2019-09-25 22:23:50 -0400

    Updates

---
+++ precache-manifest.2bd490e6d254028ac3f1c6f0a721f995.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb0d68745c3bb94278532ffa52a1f08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c5416b8ad39833aa53",
+    "url": "/static/css/main.d47127b4.chunk.css"
+  },
+  {
+    "revision": "f7d0dc12ab9d60fdcc32",
+    "url": "/static/js/2.c2840989.chunk.js"
+  },
+  {
+    "revision": "87c5416b8ad39833aa53",
+    "url": "/static/js/main.4c929cf3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d89e29b77a4807406451b30a68d8c5f0",
+    "url": "/static/media/loading.d89e29b7.png"
+  },
+  {
+    "revision": "f7d4c16ca73aaeebb7fedf8b7332fcf6",
+    "url": "/static/media/pixibayLogo.f7d4c16c.svg"
+  },
+  {
+    "revision": "85693c4d593bf38474a85f2469c89cb7",
+    "url": "/static/media/spinner.85693c4d.mp4"
+  }
+]);
